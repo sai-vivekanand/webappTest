7af9aaf42a401323ee5bed1eb3be17c5d1a70faa
sai-vivekanand <141432630+sai-vivekanand@users.noreply.github.com>
2024-02-19 16:18:20 -0500

    Update UserControllerIntegrationTests.java

--- src/test/java/com/neu/cloud/cloudApp/UserControllerIntegrationTests.java
+++ src/test/java/com/neu/cloud/cloudApp/UserControllerIntegrationTests.java
@@ -35,12 +35,12 @@ public class UserControllerIntegrationTests {
         // Create a user
         mockMvc.perform(post("/v1/user")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"username\":\"test98@example.com\",\"password\":\"password\",\"first_name\":\"Test\",\"last_name\":\"User\"}"))
+                        .content("{\"username\":\"test94@example.com\",\"password\":\"password\",\"first_name\":\"Test\",\"last_name\":\"User\"}"))
                 .andExpect(status().isCreated());
 
         // Validate account exists
         mockMvc.perform(get("/v1/user/self")
-                        .header("Authorization", "Basic " + Base64Utils.encodeToString("test@example.com:password".getBytes())))
+                        .header("Authorization", "Basic " + Base64Utils.encodeToString("test95@example.com:password".getBytes())))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.username").value("test@example.com"));
     }
